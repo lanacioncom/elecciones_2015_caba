3f3fb4179247676133737a414938c543633d1ae8
cristian Bertelegni <cristianbertelegni@gmail.com>
2015-04-08 16:22:54 -0300

    dict_cand.py

--- backend/export_dictionary.py
+++ backend/export_dictionary.py
@@ -2,3 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import json, csv
+
+INPUT_FOLDER = "./data_input"
+EXAMPLE_DIR = 'examples/json'
+
+
+f = open( "%s/candidatos.csv" % INPUT_FOLDER, 'rb')
+
+reader = csv.DictReader(f, delimiter=',')
+
+
+fo = open('%s/dict_candidatos.json' % (EXAMPLE_DIR), 'wb')
+fo.write(json.dumps(list(reader)))
+
+fo.close()
+f.close()
+
