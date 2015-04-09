b6f2805c16d663df06a711b19581821b44aa5ed5
cristian Bertelegni <cristianbertelegni@gmail.com>
2015-04-09 17:03:56 -0300

    sort data results

--- backend/export_results.py
+++ backend/export_results.py
@@ -1,72 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
+# agregar candidatos al json
+
+import json, random
+from operator import itemgetter, attrgetter, methodcaller
 
 EXAMPLE_DIR = 'examples/json'
 
-class ExportData(object):
-	"""docstring for ExportData"""
+# class ExportData(object):
+# 	"""docstring for ExportData"""
 
-	def __init__(self, arg):
-		pass
+# 	def __init__(self, arg):
+# 		pass
+
+def sort_data(a, key='porcentaje'):
+	return sorted(a, key=lambda x: x[key], reverse = True)
 
 
-
-def make_data(id = None, porcentaje = None):
+def make_data(id = None, porcentaje = 100, votos = None):
 	"""retorna el id y porcentaje para algún nodo definido"""
-	return dict(id = id, procentaje = porcentaje)
+	return dict(id = id, porcentaje = random.randrange(1, porcentaje), votos=votos)
+
+
+def make_data_comuna(cant):
+	d = sort_data([make_data(x, 10) for x in range(cant)])
+	return d
+
+
+def get_comunuas(cant):
+	"""los valores que recibe son reprecentativos"""
+	return dict(
+			# cada comuna tiene un listado de candidatos
+				comuna_1 = make_data_comuna(cant),
+				comuna_2 = make_data_comuna(cant),
+				comuna_3 = make_data_comuna(cant),
+				comuna_4 = make_data_comuna(cant),
+				comuna_5 = make_data_comuna(cant),
+				comuna_6 = make_data_comuna(cant),
+				comuna_7 = make_data_comuna(cant),
+				comuna_8 = make_data_comuna(cant),
+				comuna_9 = make_data_comuna(cant),
+				comuna_10 = make_data_comuna(cant),
+				comuna_11 = make_data_comuna(cant),
+				comuna_12 = make_data_comuna(cant),
+				comuna_13 = make_data_comuna(cant),
+				comuna_14 = make_data_comuna(cant),
+				comuna_15 = make_data_comuna(cant),
+			)
+		
 
 
 data = dict(
 	general = dict(
 		total = [make_data(x, 10) for x in range(10)],
-		comunas = dict(
-			# cada comuna tiene un listado de candidatos
-				comuna_1 = [make_data(x, 10) for x in range(10)], 
-				comuna_2 = [make_data(x, 10) for x in range(10)],
-				comuna_3 = [make_data(x, 10) for x in range(10)],
-				comuna_4 = [make_data(x, 10) for x in range(10)],
-				comuna_5 = [make_data(x, 10) for x in range(10)],
-				comuna_6 = [make_data(x, 10) for x in range(10)],
-				comuna_7 = [make_data(x, 10) for x in range(10)],
-				comuna_8 = [make_data(x, 10) for x in range(10)],
-				comuna_9 = [make_data(x, 10) for x in range(10)],
-				comuna_10 = [make_data(x, 10) for x in range(10)],
-				comuna_11 = [make_data(x, 10) for x in range(10)],
-				comuna_12 = [make_data(x, 10) for x in range(10)],
-				comuna_13 = [make_data(x, 10) for x in range(10)],
-				comuna_14 = [make_data(x, 10) for x in range(10)],
-				comuna_15 = [make_data(x, 10) for x in range(10)],
-			)
-		),
-
+		comunas = get_comunuas(10),
+	),
 	interna = dict(
 		partido_id = 1,
 		porcentaje = 20,
 		total = [make_data(55, 20), make_data(55, 20), make_data(55, 20), ],
-		comunas = dict(
-			# cada comuna tiene un listado de candidatos
-				comuna_1 = [make_data(x, 10) for x in range(3)], 
-				comuna_2 = [make_data(x, 10) for x in range(3)],
-				comuna_3 = [make_data(x, 10) for x in range(3)],
-				comuna_4 = [make_data(x, 10) for x in range(3)],
-				comuna_5 = [make_data(x, 10) for x in range(3)],
-				comuna_6 = [make_data(x, 10) for x in range(3)],
-				comuna_7 = [make_data(x, 10) for x in range(3)],
-				comuna_8 = [make_data(x, 10) for x in range(3)],
-				comuna_9 = [make_data(x, 10) for x in range(3)],
-				comuna_10 = [make_data(x, 10) for x in range(3)],
-				comuna_11 = [make_data(x, 10) for x in range(3)],
-				comuna_12 = [make_data(x, 10) for x in range(3)],
-				comuna_13 = [make_data(x, 10) for x in range(3)],
-				comuna_14 = [make_data(x, 10) for x in range(3)],
-				comuna_15 = [make_data(x, 10) for x in range(3)],
-			)
-		),
-	)
+		comunas = get_comunuas(3),
+	),
 
+	candidatos = dict(),
+)
 
 f = open('%s/results_example.json' % (EXAMPLE_DIR), 'wb')
 f.write(json.dumps(data))
 f.close()
+ 
+
