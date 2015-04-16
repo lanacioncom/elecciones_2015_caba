250cb0f58601c0150acbc93656973b1af18ecaff
cristian Bertelegni <cristianbertelegni@gmail.com>
2015-04-16 18:21:33 -0300

    x_interna

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,9 +1,30 @@
 // scripts app
+
+"use strict";
+
 (function(){
-	"use strict";
 	// load mapa
 	$.get("img/mapaBA_SVG.txt", function(mapa){
 		$("#mapa_cont").html(mapa);
 	});
 
+	$('select#opts').change(function(e){
+		change_dropdown($(this).val());
+	});
+
 })();
+
+function change_dropdown(val){
+	var x_fuerza = 'x_fuerza';
+	if(x_fuerza == val){
+		$('#x_interna').hide();
+		$('#x_fuerza').show();
+	}else{
+		$('#x_fuerza').hide();
+		$('#x_interna').show();
+	}
+}
+
+function display_none(el){
+	$(el).fadeOut();
+}
