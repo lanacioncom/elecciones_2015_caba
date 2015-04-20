22d773dde67adf193e52174e2700239aa4d9e58d
Gaston de la llana <gasdela@gmail.com>
2015-04-20 18:00:38 -0300

    cambios varios

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -18,4 +18,15 @@ $(function(){
    	 	window.open( url, "Compartir", "status = yes, height = 360, width = 500, resizable = yes, left = "+(ancho/2+250)+", top =" +(alto/2-150) );
     	return false;
 	}
+
+	/* // scroll // */
+
+	  $("#list").niceScroll({
+	        cursorcolor:"#d7d7d7",
+	        cursorborder:"0px solid #fff",
+	        cursorwidth: "7px",
+	        autohidemode:false,
+	        hidecursordelay:0
+	  }); 
+
 });
