85dbbc3e19972aaffd2614e93830263c03ac68f3
Juan Elosua <juan.elosua@gmail.com>
2015-04-24 05:22:51 -0300

    remove io for JSON export

--- api_backend/scripts/apiio.py
+++ api_backend/scripts/apiio.py
@@ -22,12 +22,15 @@ def write_API_data(d_d=None):
     '''Write the transformed JSON to disk'''
     for fname, d in d_d.iteritems():
         try:
+            # with io.open('%s/%s.json'
+            #              % (JSON_DATA_PATH, fname),
+            #              'w', encoding='utf8') as f:
             with io.open('%s/%s.json'
                          % (JSON_DATA_PATH, fname),
-                         'w', encoding='utf8') as f:
+                         'wb') as f:
                 log.debug("writing output JSON: %s.json" % (fname))
                 f.write(json.dumps(d, ensure_ascii=False))
-            return True
         except (IOError):
             log.error("Failed to save JSON file %s" % (fname))
             return False
+    return True
