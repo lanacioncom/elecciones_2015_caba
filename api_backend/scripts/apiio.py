e7c955cc5f01d41df09b6c7d093bae683a13d0fe
Juan Elosua <juan.elosua@gmail.com>
2015-04-26 02:04:44 -0300

    Refactor error handling. fixes #34

--- api_backend/scripts/apiio.py
+++ api_backend/scripts/apiio.py
@@ -2,7 +2,7 @@
 import logging
 import io
 import json
-from config import JSON_DATA_PATH
+from config import JSON_DATA_PATH, Paso2015
 log = logging.getLogger('paso.%s' % __name__)
 
 
@@ -15,29 +15,31 @@ def get_stored_json(fname=None):
         log.debug("resumen json old data: %s" % (j))
         return j
     except (IOError):
-        # First run
+        log.warning("Did not find stored json, \
+                    maybe it is the first execution")
         return None
 
 
 def write_API_data(d_d=None):
     '''Write the transformed JSON to disk'''
     for fname, d in d_d.iteritems():
-        try:
-            with io.open('%s/%s.json'
-                         % (JSON_DATA_PATH, fname),
-                         'w', encoding='utf8') as f:
-                log.debug("writing output JSON: %s.json" % (fname))
-                f.write(json.dumps(d, ensure_ascii=False))
-        except (IOError):
-            log.error("Failed to save JSON file %s" % (fname))
-            return False
-    return True
+        write_JSON_file(JSON_DATA_PATH, fname, d)
 
 
 def write_JSON_file(path=None, fname=None, data=None):
     '''Write JSON files to disk'''
-    with io.open('%s/%s.json'
-                 % (JSON_DATA_PATH, fname),
-                 'wb') as f:
-        log.debug("writing output JSON: %s.json" % (fname))
-        f.write(json.dumps(data, ensure_ascii=False))
+    try:
+        # with io.open('%s/%s.json'
+        #                  % (JSON_DATA_PATH, fname),
+        #                  'w', encoding='utf8') as f:
+        #         log.debug("writing output JSON: %s.json" % (fname))
+        #         f.write(json.dumps(d, ensure_ascii=False))
+        with io.open('%s/%s.json'
+                     % (JSON_DATA_PATH, fname),
+                     'wb') as f:
+            log.debug("writing output JSON: %s.json" % (fname))
+            f.write(json.dumps(data, ensure_ascii=False))
+    except IOError, e:
+        log.error("Failed to save JSON file %s. Reason %s"
+                  % (fname, str(e)))
+        raise Paso2015(__name__)
